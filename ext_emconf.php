be1c8bab6a6a5202cca3e102e9f07125e95b61bc
Tomas Norre Mikkelsen <tomas.mikkelsen@aoe.com>
2021-01-18 21:05:57 +0100

    initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,21 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'TNM GitHub Action Demo Extension',
+    'description' => 'TYPO3 Demo Extension for testing GitHub Actions',
+    'category' => 'module',
+    'state' => 'beta',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'author' => 'Tomas Norre Mikkelsen',
+    'author_email' => '',
+    'author_company' => '',
+    'version' => '0.0.1',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '11.0.0-11.5.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ]
+];
